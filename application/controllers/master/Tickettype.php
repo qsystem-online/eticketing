9bddf0d44712d92e2254961b864f6792fc2765d1
Enny <enny.nuraini06@gmail.com>
2020-01-09 14:13:43 +0700

    enny06 2020/01/09 14.13 ticket type

--- application/controllers/master/Tickettype.php
+++ application/controllers/master/Tickettype.php
@@ -39,7 +39,15 @@ class Tickettype extends MY_Controller
         $this->list['columns'] = [
             ['title' => 'Ticket Type ID', 'width' => '25%', 'data' => 'fin_ticket_type_id'],
             ['title' => 'Ticket Type Name', 'width' => '40%', 'data' => 'fst_ticket_type_name'],
-            ['title' => 'Assignment or Notice', 'width' => '25%', 'data' => 'fst_assignment_or_notice', 'className' => 'dt-body-center text-center'],
+            ['title' => 'Assignment or Notice', 'width' => '25%', 'data' => 'fst_assignment_or_notice', 'className' => 'text-center',
+                'render' =>"function(data,type,row){
+                    if(data == 'A'){
+                        return 'Assignment';
+                    }else if(data == 'N'){
+                        return 'Notice';
+                    }
+                }"
+            ],
             ['title' => 'Action', 'width' => '10%', 'data' => 'action', 'sortable' => false, 'className' => 'dt-body-center text-center']
         ];
         $main_header = $this->parser->parse('inc/main_header', [], true);
