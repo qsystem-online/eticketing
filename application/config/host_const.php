6d19768f30c436664fcf9e35dce6656a7db66035
Qsystem <devibong@qsystem-online.com>
2020-02-05 08:40:42 +0700

    devibong 2020-02-05

--- application/config/host_const.php
+++ application/config/host_const.php
@@ -1,5 +1,5 @@
 <?php
 
-defined('BASE_URL') OR define ('BASE_URL','http://localhost/eticketing');
+defined('BASE_URL') OR define ('BASE_URL','http://eticketing.qsystem-online.com/');
 #defined('COMPONENT_URL') OR define ('COMPONENT_URL','http://localhost/component/');
 defined('COMPONENT_URL') OR define ('COMPONENT_URL','http://greebel.qsystem-online.com/');
