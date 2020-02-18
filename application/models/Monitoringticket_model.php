92bb5a44f412e707c0388b2ee56caf6c8f0bb944
Bahroni <m.bahroni86@gmail.com>
2020-02-18 13:13:15 +0700

    @Roni Monitoring ticket 20200218 13:13

--- application/models/Monitoringticket_model.php
+++ application/models/Monitoringticket_model.php
@@ -13,8 +13,8 @@ class Monitoringticket_model extends MY_MODEL {
 
         $ssql = "SELECT a.*,b.fin_user_id,b.fst_username AS issuedBy,b.fin_department_id as produksi,c.fst_username AS issuedTo,d.fst_username AS Approved FROM trticket a 
         INNER JOIN users b ON a.fin_issued_by_user_id = b.fin_user_id
-        INNER JOIN users c ON a.fin_issued_to_user_id = c.fin_user_id
-        INNER JOIN users d ON a.fin_approved_by_user_id = d.fin_user_id
+        LEFT JOIN users c ON a.fin_issued_to_user_id = c.fin_user_id
+        LEFT JOIN users d ON a.fin_approved_by_user_id = d.fin_user_id
         WHERE (b.fin_department_id IN ? OR c.fin_department_id IN ?) AND a.fst_status != 'CLOSED' AND a.fst_status != 'REJECTED' AND a.fst_status != 'VOID' AND a.fst_status != 'TICKET_EXPIRED' ORDER BY a.fdt_ticket_datetime ";
         $qr = $this->db->query($ssql,[$arrDepartment,$arrDepartment]);
         //echo $this->db->last_query();
@@ -22,17 +22,33 @@ class Monitoringticket_model extends MY_MODEL {
         //return $qr->result_array();
         $rwtickets = $qr->result();
 
-        $ssql = "SELECT a.fst_memo FROM trticket a
+        /*$ssql = "SELECT a.fst_memo FROM trticket a
         INNER JOIN mstickettype b ON b.fin_ticket_type_id = a.fin_ticket_type_id
-        WHERE b.fst_assignment_or_notice = 'INFO' ORDER BY a.fin_ticket_id desc";
+        WHERE b.fst_assignment_or_notice = 'INFO' AND a.fst_status ='APPROVED/OPEN' ORDER BY a.fin_ticket_id desc";
         $qr = $this->db->query($ssql, []);
         //echo $this->db->last_query();
         //die();
-        $rwInfo = $qr->result();
+        $rwInfo = $qr->result();*/
 
         $data = [
             "tickets" => $rwtickets,
-            "arrPengumuman" => $rwInfo
+        ];
+        return $data;
+    }
+
+    public function get_pengumuman(){
+
+        $ssql = "SELECT a.* FROM trticket a
+        INNER JOIN mstickettype b ON b.fin_ticket_type_id = a.fin_ticket_type_id
+        ORDER BY a.fin_ticket_id desc";
+        /*WHERE b.fst_assignment_or_notice = 'INFO' AND a.fst_status ='APPROVED/OPEN' ORDER BY a.fin_ticket_id desc";*/
+        $qr = $this->db->query($ssql, []);
+        //echo $this->db->last_query();
+        //die();
+        $rwInfo = $qr->result_array();
+
+        $data = [
+            "arrPengumuman" => $rwInfo,
         ];
         return $data;
     }
