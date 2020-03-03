ec472db2757257e7635c74d1bda883d00803c706
Enny <enny.nuraini06@gmail.com>
2020-03-03 18:04:22 +0700

    enny06 03/03/2020 18.03 add ticket docs

--- application/views/pages/tr/ticket/form.php
+++ application/views/pages/tr/ticket/form.php
@@ -825,7 +825,7 @@ defined('BASEPATH') or exit ('No direct script access allowed');
                 $.each(resp.ms_ticketdocs, function(name, val) {
                     console.log(val);
                         var tbody = '<tr>';
-                            tbody += '<td style="width:30%"><a href="<?=site_url()?>/assets/app/tickets/image/13.jpg">'+val.fst_doc_title+'</a></td>';
+                            tbody += '<td style="width:30%"><a href="<?=site_url()?>/assets/app/tickets/image/14.jpg">'+val.fst_doc_title+'</a></td>';
                             tbody += '<td style="width:50%">'+val.fst_memo+'</td>';
                             tbody += '<td style="width:30%">'+val.fdt_insert_datetime+'</td>';
                         tbody += '</tr>';
