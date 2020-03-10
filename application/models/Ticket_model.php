c265837be70e7769539af5a9c5ca9efe8542694f
Enny <enny.nuraini06@gmail.com>
2020-03-10 09:43:07 +0700

    enny06 2020/03/10 09.42 modified ticket model (ticket docs)

--- application/models/Ticket_model.php
+++ application/models/Ticket_model.php
@@ -33,7 +33,7 @@ class Ticket_model extends MY_MODEL {
 
         // Ticket Docs 27/02/2020
         $ssql = "SELECT a.*,b.fin_ticket_id,b.fst_status_memo FROM trticket_docs a
-        INNER JOIN trticket_log b ON a.fst_memo = b.fst_status_memo 
+        LEFT JOIN trticket_log b ON a.fst_memo = b.fst_status_memo 
         WHERE b.fin_ticket_id = ? ORDER BY a.fin_rec_id DESC";
         $qr = $this->db->query($ssql, [$fin_ticket_id]);
         $rsTicketDocs = $qr->result();
--- application/models/Ticketstatus_model.php
+++ application/models/Ticketstatus_model.php
@@ -39,7 +39,7 @@ class Ticketstatus_model extends MY_MODEL {
 
         // Ticket Lampiran 27/02/2020 enny
         $ssql = "SELECT a.*,b.fin_ticket_id,b.fst_status_memo FROM trticket_docs a
-        INNER JOIN trticket_log b ON a.fst_memo = b.fst_status_memo 
+        LEFT JOIN trticket_log b ON a.fst_memo = b.fst_status_memo 
         WHERE b.fin_ticket_id = ? ORDER BY a.fin_rec_id DESC";
         $qr = $this->db->query($ssql, [$fin_ticket_id]);
         $rsTicketDocs = $qr->result();
