3dfe10b8da7dee0865af9dce6a9df25c08fc899c
Enny <enny.nuraini06@gmail.com>
2020-03-20 13:49:35 +0700

    enny06 20/03/2020 13.50 modified ticket form

--- application/views/pages/tr/ticket/form.php
+++ application/views/pages/tr/ticket/form.php
@@ -744,8 +744,13 @@ defined('BASEPATH') or exit ('No direct script access allowed');
                     }
                 });
 
-                $("#fdt_ticket_datetime").datetimepicker('update', dateTimeFormat(resp.ms_ticket.fdt_ticket_datetime));
-                $("#fdt_acceptance_expiry_datetime").datetimepicker('update', dateTimeFormat(resp.ms_ticket.fdt_acceptance_expiry_datetime));
+                if (resp.ms_ticket.fdt_deadline_extended_datetime != null) {
+                    $("#fdt_deadline_extended_datetime").datetimepicker('update', dateTimeFormat(resp.ms_ticket.fdt_deadline_extended_datetime));
+                    $("#fdt_ticket_datetime").datetimepicker('update', dateTimeFormat(resp.ms_ticket.fdt_ticket_datetime));
+                    $("#fdt_acceptance_expiry_datetime").datetimepicker('update', dateTimeFormat(resp.ms_ticket.fdt_acceptance_expiry_datetime));
+                } else {
+                    $("#fdt_deadline_extended_datetime").datetimepiker(null);
+                }
 
                 var newOption = new Option(resp.ms_ticket.fst_ticket_type_name, true);
                 $('#select-ticketType').append(newOption).trigger('change');
